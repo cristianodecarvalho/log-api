c672f5088b7db912506861590b6c2fd6ee716bc8
Cristiano de Carvalho <cristianfilho18@gmail.com>
2021-10-27 22:05:40 -0300

    Boas práticas para trabalhar com data hora

--- src/main/java/com/cristianofilho/log/domain/model/Entrega.java
+++ src/main/java/com/cristianofilho/log/domain/model/Entrega.java
@@ -1,7 +1,7 @@
 package com.cristianofilho.log.domain.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
@@ -52,9 +52,9 @@ public class Entrega {
 	private StatusEntrega status;
 	
 	//@JsonProperty(access = Access.READ_ONLY)
-	private LocalDateTime dataPedido;
+	private OffsetDateTime dataPedido;
 	
 	//@JsonProperty(access = Access.READ_ONLY)
-	private LocalDateTime dataFinalizacao;
+	private OffsetDateTime dataFinalizacao;
 	
 }
