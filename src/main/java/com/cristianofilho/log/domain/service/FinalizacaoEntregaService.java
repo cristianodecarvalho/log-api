74406e3f8f7586999e2097accbd205dd03bc1572
Cristiano de Carvalho <cristianfilho18@gmail.com>
2021-11-01 11:32:51 -0300

    Implementando ação não CRUD

---
+++ src/main/java/com/cristianofilho/log/domain/service/FinalizacaoEntregaService.java
@@ -0,0 +1,28 @@
+package com.cristianofilho.log.domain.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.cristianofilho.log.domain.model.Entrega;
+import com.cristianofilho.log.domain.repository.EntregaRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class FinalizacaoEntregaService {
+
+	private EntregaRepository entregaRepository;
+	private BuscaEntregaService buscaEntregaService;
+	
+	@Transactional
+	public void finalizar(Long entregaId) {
+		Entrega entrega = buscaEntregaService.buscar(entregaId);
+		
+		entrega.finalizar();
+		
+		entregaRepository.save(entrega);
+	}
+	
+}
